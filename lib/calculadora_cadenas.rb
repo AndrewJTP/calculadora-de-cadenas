f43fac0c2107a7a134854a95dbc93cfd278379ff
Andrew <andrew.j.t.p@gmail.com>
2021-04-28 17:31:51 -0400

    devuelve correctamente 2 numeros

---
+++ lib/calculadora_cadenas.rb
@@ -0,0 +1,4 @@
+def calCadena(numCa)
+    arraynums=numCa.split(",")
+    return arraynums[0].to_i+arraynums[1].to_i
+end
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -1,5 +1,12 @@
+require "./lib/calculadora_cadenas.rb"
 RSpec.describe "calculadora de cadenas" do
-    it "" do
-        expect(true).to eq true
+    it "deberia devolver el numero 3 si le pasamos la cadena '3'" do
+        expect(calCadena("3")).to eq 3
+    end
+    it "deberia devolver el numero 5 si le pasamos la cadena '5'" do
+        expect(calCadena("5")).to eq 5
+    end
+    it "deberia devolver el numero 8 si le pasamos la cadena '5,3'" do
+        expect(calCadena("5,3")).to eq 8
     end
 end
