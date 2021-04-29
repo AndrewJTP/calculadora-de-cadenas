71d376f05fc86f83378196e08a30739684040dbe
Andrew <andrew.j.t.p@gmail.com>
2021-04-29 08:49:39 -0400

    refactorizacion del metodo sumar

--- lib/calculadora_cadenas.rb
+++ lib/calculadora_cadenas.rb
@@ -1,13 +1,21 @@
-def calCadena(numCa)
-    if (numCa.split(/\n/)[0].include?"//")
-        delimitadores=((numCa.split(/\n/)[0].gsub"/","").gsub "[","").gsub "]",""
-        arraynums=numCa.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
-        
+def sumar(cadena_numeros)
+    if (existen_delimitadores(cadena_numeros))  
+        delimitadores=obtener_delimitadores(cadena_numeros)
+        numeros=obtener_numeros(cadena_numeros, delimitadores)
     else
-        arraynums=numCa.split(/[,\s]/)
+        numeros=cadena_numeros.split(/[,\s]/)
     end
-    arraynumsInt=arraynums.map{|i| i.to_i if i.to_i<1001 }.compact
-    suma=arraynumsInt.sum()
+    numerosInt=numeros.map{|i| i.to_i if i.to_i<1001 }.compact
+    suma=numerosInt.sum()
     return suma
     
+end
+def obtener_delimitadores(cadena_numeros)
+    ((cadena_numeros.split(/\n/)[0].gsub"/","").gsub "[","").gsub "]",""
+end
+def obtener_numeros(cadena_numeros, delimitadores)
+    cadena_numeros.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
+end
+def existen_delimitadores(cadena_numeros)
+    cadena_numeros.split(/\n/)[0].include?"//"   
 end
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -1,32 +1,31 @@
 require "./lib/calculadora_cadenas.rb"
 RSpec.describe "calculadora de cadenas" do
     it "deberia devolver el numero 3 si le pasamos la cadena '3'" do
-        expect(calCadena("3")).to eq 3
+        expect(sumar("3")).to eq 3
     end
     it "deberia devolver el numero 5 si le pasamos la cadena '5'" do
-        expect(calCadena("5")).to eq 5
+        expect(sumar("5")).to eq 5
     end
     it "deberia devolver el numero 8 si le pasamos la cadena '5,3'" do
-        expect(calCadena("5,3")).to eq 8
+        expect(sumar("5,3")).to eq 8
     end
     it "deberia devolver el numero 10 si le pasamos la cadena '5,3,2'" do
-        expect(calCadena("5,3,2")).to eq 10
+        expect(sumar("5,3,2")).to eq 10
     end
     it "deberia devolver el numero 15 si le pasamos la cadena '5 7,3'" do
-        expect(calCadena("5 7,3")).to eq 15
+        expect(sumar("5 7,3")).to eq 15
     end
     it "deberia devolver el numero 17 si le pasamos la cadena '//[;]\n 6;7;4'" do
-        expect(calCadena("//[;]\n6;7;4")).to eq 17
+        expect(sumar("//[;]\n6;7;4")).to eq 17
     end
     it "deberia devolver el numero 15 si le pasamos la cadena '//[***][%]\n8***3%4'" do
-        expect(calCadena("//[***][%]\n8***3%4")).to eq 15
+        expect(sumar("//[***][%]\n8***3%4")).to eq 15
     end
     it "deberia devolver el numero 17 si le pasamos la cadena '//[**][%][&&]\n8&&3%4**2'" do
-        expect(calCadena("//[**][%][&&]\n8&&3%4**2")).to eq 17
+        expect(sumar("//[**][%][&&]\n8&&3%4**2")).to eq 17
     end
     it "deberia devolver el numero 17 si le pasamos la cadena '//[**][%][&&]\n8&&3%4**2**1001'" do
-        expect(calCadena("//[**][%][&&]\n8&&3%4**2**1001")).to eq 17
-    end
-    
+        expect(sumar("//[**][%][&&]\n8&&3%4**2**1001")).to eq 17
+    end   
     
 end
