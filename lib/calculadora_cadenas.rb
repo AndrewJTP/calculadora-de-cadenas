2ed58f2ee06a2bd4799884c15bcb8582e04580e3
Andrew <andrew.j.t.p@gmail.com>
2021-04-28 22:01:28 -0400

    calculadora de cadenas funcinal

--- lib/calculadora_cadenas.rb
+++ lib/calculadora_cadenas.rb
@@ -1,4 +1,13 @@
 def calCadena(numCa)
-    arraynums=numCa.split(",")
-    return arraynums[0].to_i+arraynums[1].to_i
+    if (numCa.split(/\n/)[0].include?"//")
+        delimitadores=((numCa.split(/\n/)[0].gsub"/","").gsub "[","").gsub "]",""
+        arraynums=numCa.split(/\n/)[1].split(Regexp.union(delimitadores.split("")))
+        
+    else
+        arraynums=numCa.split(/[,\s]/)
+    end
+    arraynumsInt=arraynums.map{|i| i.to_i if i.to_i<1001 }.compact
+    suma=arraynumsInt.sum()
+    return suma
+    
 end
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe "calculadora de cadenas" do
     it "deberia devolver el numero 8 si le pasamos la cadena '5,3'" do
         expect(calCadena("5,3")).to eq 8
     end
+    it "deberia devolver el numero 10 si le pasamos la cadena '5,3,2'" do
+        expect(calCadena("5,3,2")).to eq 10
+    end
+    it "deberia devolver el numero 15 si le pasamos la cadena '5 7,3'" do
+        expect(calCadena("5 7,3")).to eq 15
+    end
+    it "deberia devolver el numero 17 si le pasamos la cadena '//[;]\n 6;7;4'" do
+        expect(calCadena("//[;]\n6;7;4")).to eq 17
+    end
+    it "deberia devolver el numero 15 si le pasamos la cadena '//[***][%]\n8***3%4'" do
+        expect(calCadena("//[***][%]\n8***3%4")).to eq 15
+    end
+    it "deberia devolver el numero 17 si le pasamos la cadena '//[**][%][&&]\n8&&3%4**2'" do
+        expect(calCadena("//[**][%][&&]\n8&&3%4**2")).to eq 17
+    end
+    it "deberia devolver el numero 17 si le pasamos la cadena '//[**][%][&&]\n8&&3%4**2**1001'" do
+        expect(calCadena("//[**][%][&&]\n8&&3%4**2**1001")).to eq 17
+    end
+    
+    
 end
